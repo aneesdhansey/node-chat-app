a575e59e3642a8b5a716b4d43977df59f7bc3f59
Anees Dhansey <aneesdhansey@gmail.com>
2018-04-03 13:02:00 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected...');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app...',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected...');
     });
@@ -24,22 +36,14 @@ io.on('connection', (socket) => {
         console.log('createEmail', newEmail);
     });
 
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey. What is going on.',
-    //     createdAt: new Date().getTime()
-    // });
+
 
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
         io.emit('newMessage', { ...message, createdAt: new Date().getTime() });
-    });
 
-    // socket.emit('newMessage', {
-    //     from: 'fluffy@gmail.com',
-    //     text: 'Hi from fluffy',
-    //     createdAt: new Date().getTime()
-    // });
+        //socket.broadcast.emit('newMessage', {...message, createdAt: new Date().getTime()})
+    });
 });
 
 server.listen(port, () => console.log(`Server is up on port ${port}...`));
